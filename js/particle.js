3edb7428ff5eb9ed578d75f487c593c411e83c80
Daniel194 <lungu.daniel94@gmail.com>
2019-06-04 21:21:55 +0300

    add particle and cleanup - WIP

--- js/particle.js
+++ js/particle.js
@@ -1,6 +1,6 @@
 particlesJS("particles-js", {
   "particles": {
-    "number": {"value": 80, "density": {"enable": true, "value_area": 800}},
+    "number": {"value": 100, "density": {"enable": true, "value_area": 800}},
     "color": {"value": "#000000"},
     "shape": {
       "type": "circle",
@@ -13,11 +13,11 @@ particlesJS("particles-js", {
       "random": false,
       "anim": {"enable": false, "speed": 1, "opacity_min": 0.1, "sync": false}
     },
-    "size": {"value": 3, "random": true, "anim": {"enable": false, "speed": 40, "size_min": 0.1, "sync": false}},
-    "line_linked": {"enable": true, "distance": 150, "color": "#000000", "opacity": 0.4, "width": 1},
+    "size": {"value": 10, "random": true, "anim": {"enable": false, "speed": 40, "size_min": 1, "sync": false}},
+    "line_linked": {"enable": true, "distance": 150, "color": "#000000", "opacity": 0.4, "width": 5},
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 3,
       "direction": "none",
       "random": false,
       "straight": false,
@@ -35,7 +35,7 @@ particlesJS("particles-js", {
     },
     "modes": {
       "grab": {"distance": 400, "line_linked": {"opacity": 1}},
-      "bubble": {"distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3},
+      "bubble": {"distance": 400, "size": 400, "duration": 2, "opacity": 8, "speed": 3},
       "repulse": {"distance": 200, "duration": 0.4},
       "push": {"particles_nb": 4},
       "remove": {"particles_nb": 2}
