4d717b023331e181f0de9f1988b3cbaca4394673
Daniel194 <lungu.daniel94@gmail.com>
2019-06-22 22:40:24 +0300

    cleanup

--- js/main.js
+++ js/main.js
@@ -46,8 +46,8 @@
 
 
   var counterWayPoint = function () {
-    if ($('#colorlib-counter').length > 0) {
-      $('#colorlib-counter').waypoint(function (direction) {
+    if ($('#website-counter').length > 0) {
+      $('#website-counter').waypoint(function (direction) {
 
         if (direction === 'down' && !$(this.element).hasClass('animated')) {
           setTimeout(counter, 400);
@@ -97,7 +97,7 @@
 
   var burgerMenu = function () {
 
-    $('.js-colorlib-nav-toggle').on('click', function (event) {
+    $('.js-website-nav-toggle').on('click', function (event) {
       event.preventDefault();
       var $this = $(this);
 
@@ -117,13 +117,13 @@
   var mobileMenuOutsideClick = function () {
 
     $(document).click(function (e) {
-      var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
+      var container = $("#website-aside, .js-website-nav-toggle");
       if (!container.is(e.target) && container.has(e.target).length === 0) {
 
         if ($('body').hasClass('offcanvas')) {
 
           $('body').removeClass('offcanvas');
-          $('.js-colorlib-nav-toggle').removeClass('active');
+          $('.js-website-nav-toggle').removeClass('active');
 
         }
 
@@ -134,7 +134,7 @@
       if ($('body').hasClass('offcanvas')) {
 
         $('body').removeClass('offcanvas');
-        $('.js-colorlib-nav-toggle').removeClass('active');
+        $('.js-website-nav-toggle').removeClass('active');
 
       }
     });
@@ -156,7 +156,7 @@
       if (navbar.is(':visible')) {
         navbar.removeClass('in');
         navbar.attr('aria-expanded', 'false');
-        $('.js-colorlib-nav-toggle').removeClass('active');
+        $('.js-website-nav-toggle').removeClass('active');
       }
 
       event.preventDefault();
@@ -205,7 +205,7 @@
 
   var sliderMain = function () {
 
-    $('#colorlib-hero .flexslider').flexslider({
+    $('#website-hero .flexslider').flexslider({
       animation: "fade",
       slideshowSpeed: 5000,
       directionNav: true,
